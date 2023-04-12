b4b7f1501a238b08171bcdb716ce48b2e5467881
Mersmith <mersmith14@gmail.com>
2023-04-12 18:17:20 -0500

    Menu Principal y sidebar.

---
+++ resources/views/web/menu/menu-pie.blade.php
@@ -0,0 +1,10 @@
+<div class="contenedor_navbar_pie">
+    <div class="navbar_pie_menu">
+        <a href="">Servicios</a>
+        <a href="">Sedes</a>
+        <a href="">¿Qué es CRD Puntos?</a>
+        <a href="">Contacto</a>
+        <a href="" class="boton_menu_principal">Orden</a>
+        <a href="" class="boton_menu_principal">CRD Cloud</a>
+    </div>
+</div>
